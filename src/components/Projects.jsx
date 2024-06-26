ce7dd63ca4c7107c09152fed993373467f70be7a
Mattia-Franchini <mattia.franchini005@gmail.com>
2024-06-26 15:11:34 +0200

    Created Projects component

---
+++ src/components/Projects.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Projects = () => {
+  return (
+    <div id="projects" className="h-screen bg-gradient-to-r from-gray-800 via-gray-700 to-gray-900 flex items-center justify-center">
+      <div className="text-center">
+        <h2 className="text-4xl text-white font-bold mb-4">Projects</h2>
+        <p className="text-lg text-white mb-8">Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+      </div>
+    </div>
+  );
+};
+
+export default Projects;
