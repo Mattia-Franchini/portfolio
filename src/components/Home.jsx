9393f2660a8205588e9c18f2e3228128b0050d96
Mattia-Franchini <mattia.franchini005@gmail.com>
2024-06-26 15:10:57 +0200

    Changed Color Palette

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { FaCode } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div id="home" className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black p-4">
+    <div id="home" className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-gray-700 p-4">
       <div className="flex flex-col md:flex-row items-center">
         <div className="text-left text-white mb-8 md:mb-0 md:mr-24">
           <h1 className="text-3xl md:text-7xl font-bold mb-4 animate-fade-in-down">
