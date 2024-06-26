0a85f4dd6706400783a2d312cf30e578ad1af37b
Mattia-Franchini <mattia.franchini005@gmail.com>
2024-06-26 09:08:59 +0200

    Added text to homepage and some animations

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,15 +1,21 @@
 import React from "react";
+import { FaCode } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="flex items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black">
-      <div className="text-center text-white">
-        <h1 className="text-5xl font-bold mb-4 animate-fade-in-down">
-          Hi, I'm Mattia Franchini
-        </h1>
-        <p className="text-2xl animate-fade-in-up-delayed">
-          WEB DEVELOPER
-        </p>
+    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black p-4">
+      <div className="flex flex-col md:flex-row items-center">
+        <div className="text-left text-white mb-8 md:mb-0 md:mr-24">
+          <h1 className="text-4xl md:text-5xl font-bold mb-4 animate-fade-in-down">
+            Hi, I'm Mattia Franchini 
+          </h1>
+          <p className="text-xl md:text-2xl animate-fade-in-up-delayed">
+            WEB DEVELOPER
+          </p>
+        </div>
+        <div className="animate-bounce">
+          <FaCode className="text-7xl md:text-9xl text-white" />
+        </div>
       </div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,10 +11,21 @@ module.exports = {
           "0%": { opacity: 0, transform: "translateY(20px)" },
           "100%": { opacity: 1, transform: "translateY(0)" },
         },
+        bounce: {
+          "0%, 100%": {
+            transform: "translateY(-25%)",
+            animationTimingFunction: "cubic-bezier(0.8, 0, 1, 1)",
+          },
+          "50%": {
+            transform: "translateY(0)",
+            animationTimingFunction: "cubic-bezier(0, 0, 0.2, 1)",
+          },
+        },
       },
       animation: {
         "fade-in-down": "fade-in-down 0.5s ease-out",
-        "fade-in-up-delayed": "fade-in-up 0.5s ease-out",
+        "fade-in-up": "fade-in-up 0.5s ease-out",
+        bounce: "bounce 1.5s infinite",
       },
     },
   },
