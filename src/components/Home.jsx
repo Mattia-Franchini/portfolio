8d632375e54b37765b19046b55754e48500d28dc
Mattia-Franchini <mattia.franchini005@gmail.com>
2024-06-26 14:28:28 +0200

    Changes some font sizes

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,13 +3,13 @@ import { FaCode } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black p-4">
+    <div id="home" className="flex flex-col items-center justify-center h-screen bg-gradient-to-r from-gray-800 via-gray-900 to-black p-4">
       <div className="flex flex-col md:flex-row items-center">
         <div className="text-left text-white mb-8 md:mb-0 md:mr-24">
-          <h1 className="text-2xl md:text-7xl font-bold mb-4 animate-fade-in-down">
+          <h1 className="text-3xl md:text-7xl font-bold mb-4 animate-fade-in-down">
             Hi, I'm Mattia Franchini
           </h1>
-          <p className="text-xl md:text-4xl animate-fade-in-up">
+          <p className="text-2xl md:text-4xl animate-fade-in-up">
             WEB DEVELOPER
           </p>
         </div>
