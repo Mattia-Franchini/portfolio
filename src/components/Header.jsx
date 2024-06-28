6629c7e6eaf15b2081681be2740cb9800b97ba5e
Mattia-Franchini <mattia.franchini005@gmail.com>
2024-06-28 09:35:02 +0200

    Fixed z-index

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
   };
 
   return (
-    <nav className="fixed top-0 left-0 right-0 bg-white border-gray-200 dark:bg-gray-800 h-16">
+    <nav className="fixed top-0 left-0 right-0 bg-white border-gray-200 dark:bg-gray-800 h-16 z-50">
       <div className="flex flex-wrap items-center justify-between mx-auto p-4">
         <a className="flex items-center space-x-3 rtl:space-x-reverse">
           <img
